005c2472c2e797a7fe7714dda61f1c0dba8a72e6
Mateusz Bednarczyk <91325367+MateuszBednarczyk@users.noreply.github.com>
2022-04-20 18:45:25 +0200

    UPDATE: worked on error handling for register and login and code structure

--- src/main/java/com/maticendrak/socialmediaproject/RequestsDTOs/LoginAndRegisterRequest.java
+++ src/main/java/com/maticendrak/socialmediaproject/RequestsDTOs/LoginAndRegisterRequest.java
@@ -1,10 +1,12 @@
 package com.maticendrak.socialmediaproject.RequestsDTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Data
+@AllArgsConstructor
 public class LoginAndRegisterRequest {
 
     private String username;
--- src/main/java/com/maticendrak/socialmediaproject/ResponsesDTOs/LoginResponse.java
+++ src/main/java/com/maticendrak/socialmediaproject/ResponsesDTOs/LoginResponse.java
@@ -1,23 +1,17 @@
 package com.maticendrak.socialmediaproject.ResponsesDTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.mapping.List;
 
 @Data
+@AllArgsConstructor
 public class LoginResponse {
+
     private String username;
     private String description;
     private String image;
     private List posts;
     private List following;
 
-    public LoginResponse(String username, String description, String image, List posts, List following) {
-        this.username = username;
-        this.description = description;
-        this.image = image;
-        this.posts = posts;
-        this.following = following;
-    }
 }
