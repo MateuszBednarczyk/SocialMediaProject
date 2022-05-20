74587fd447cfa0fd9ca96818b386dd6d8ed47642
Mateusz Bednarczyk <91325367+MateuszBednarczyk@users.noreply.github.com>
2022-05-20 23:24:53 +0200

    UPDATE: little code refactor, new function (searching by username). first commit since long, cuz of school :(

--- src/main/java/com/maticendrak/socialmediaproject/appuser/AppUserRepository.java
+++ src/main/java/com/maticendrak/socialmediaproject/appuser/AppUserRepository.java
@@ -1,13 +1,11 @@
 package com.maticendrak.socialmediaproject.appuser;
 
-import com.maticendrak.socialmediaproject.appuser.AppUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public
-interface AppUserRepository extends JpaRepository<AppUserEntity, Long> {
+public interface AppUserRepository extends JpaRepository<AppUserEntity, Long> {
     UserDetails findAppUserEntityByUsername(String username);
 
     void deleteAppUserEntityByUsername(String username);
--- src/main/java/com/maticendrak/socialmediaproject/appuser/baseappfunctionalities/FollowService.java
+++ src/main/java/com/maticendrak/socialmediaproject/appuser/baseappfunctionalities/FollowService.java
@@ -1,4 +1,25 @@
 package com.maticendrak.socialmediaproject.appuser.baseappfunctionalities;
 
-public class FollowService {
+import com.maticendrak.socialmediaproject.appuser.AppUserRepository;
+import com.maticendrak.socialmediaproject.appuser.dtos.responses.UserResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+class FollowService {
+
+    private final AppUserRepository appUserRepository;
+
+    public ResponseEntity<UserResponseDTO> followUser() {
+        ResponseEntity<UserResponseDTO> respone;
+
+        respone = new ResponseEntity<>(HttpStatus.CONTINUE);
+
+        return respone;
+
+    }
+
 }
